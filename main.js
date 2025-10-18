aa3e0622ade2d099090b3444bb405969aae7c9e7
david <davideesspinola@gmail.com>
2025-10-18 19:44:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const usersTable = document.getElementById('usersTable')
 const cancelEdit = document.getElementById('cancelEdit')
 
 // Función para cargar usuarios
-async function loadUsers() {
+function loadUsers() {
   const { data, error } = await supabase.from('usuarios').select('*')
   if (error) return console.error(error)
 
